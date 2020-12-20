79493851c23cfb5083dddc2f04592f1f37162897
nagi125 <nagi125.dev@gmail.com>
2020-12-20 16:58:47 +0900

    開発用にCORS設定変更

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+    ],
 
     'allowed_origins_patterns' => [],
 
