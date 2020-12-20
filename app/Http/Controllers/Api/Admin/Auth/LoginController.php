495d7085b2e295a071375bd4ebbc1b8c3da2a7c6
nagi125 <nagi125.dev@gmail.com>
2020-12-20 21:48:20 +0900

    LoginControllerの作成

---
+++ app/Http/Controllers/Api/Admin/Auth/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class LoginController
+ * @package App\Http\Controllers\Api\Admin\Auth
+ */
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * @Method POST
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    protected function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // ログイン成功処理
+        if ($this->attemptLogin($request, $credentials)) {
+            $request->session()->regenerate();
+            $this->clearLoginAttempts($request);
+
+            return response()->json(['message' => 'OK!'], 200);
+        }
+
+        return response()->json(['message' => 'ユーザーが見つかりません。'], 422);
+    }
+
+    /**
+     * @param  Request  $request
+     * @param $credentials
+     * @return mixed
+     */
+    protected function attemptLogin(Request $request, $credentials)
+    {
+        return Auth::guard('api')->attempt($credentials, $request->filled('remember'));
+    }
+
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
+    protected function logout(Request $request)
+    {
+        Auth::guard('api')->logout();
+
+        $request->session()->invalidate();
+
+        return response()->json(['message' => 'done logout'], 200);
+    }
+}
