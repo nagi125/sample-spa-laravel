f3c2b5d08b18da4b1f805fbf2900d097c10c5d68
nagi125 <nagi125.dev@gmail.com>
2020-12-20 22:05:37 +0900

    サンプル用のroute追加

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Api\Admin\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// 管理画面の機能として実装するのでPrefixにadminをつける
+Route::prefix('admin')->group(function() {
+    // 認証処理
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [LoginController::class, 'login']);
+    });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/user', function () {
+            return Auth::user();
+        });
+    });
 });
+
